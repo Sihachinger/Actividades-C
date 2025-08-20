dee8ddb8ee32cec10471e59adbd51926026ed687
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-20 14:21:38 -0300

    Create TP1_B10.c

---
+++ TP1_B10.c
@@ -0,0 +1,2 @@
+//Una concesionaria de autos desea liquidar el sueldo a cada vendedor pagando $500 por mes más un plus del 10% del precio sobre cada vehículo vendido y un valor constante de 50 pesos por cada uno de ellos. Se ingresa el valor del vehículo y cuántos vehículos de ese tipo vendió. Calcular su sueldo e imprimirlo.
+

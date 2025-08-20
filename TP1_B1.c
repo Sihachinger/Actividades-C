8734a47bd52ba398fef471e90e57a360498fcf01
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-20 14:46:55 -0300

    Added integer validation

--- TP1_B1.c
+++ TP1_B1.c
@@ -1,13 +1,20 @@
-// B1. Ingresar dos valores enteros e imprimir esta suma.
+// B1. Ingresar dos valores enteros e imprimir la suma.
 
 #include <stdio.h>
 
 int main () {
-    int num1, num2, suma;
+    int num1, num2, suma, validacion;
     printf("Ejercicio 1: Sumar dos valores enteros\n");
-    printf("Ingrese dos valores enteros: ");
-    scanf("%d %d", &num1, &num2);
+    do {
+        printf("Ingrese dos valores enteros: ");
+        validacion = scanf("%d %d", &num1, &num2);
+        if (validacion != 2)
+        {
+            printf("Los números ingresados no son enteros, intentelo nuevamente.");
+            while (getchar() != '\n');
+        }
+    } while (validacion != 2);
     suma = num1 + num2;
-    printf("La suma de %d y %d es %d\n\n", num1, num2, suma);
+    printf("La suma de %d y %d es %d\n", num1, num2, suma);
     return 0;
 }

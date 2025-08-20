c74ba9800659f45233ea260fe6d5a8443d85a789
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-20 14:52:34 -0300

    Shortened code

--- TP1_B8.c
+++ TP1_B8.c
@@ -4,13 +4,12 @@
 
 int main() {
     printf("Ejercicio 8: Calcular salario de un operario\n");
-    float tiempo_trabajado;
-    float valor_hora;
+    float tiempo_trabajado, valor_hora, sueldo;
     printf("Ingrese el tiempo trabajado (en horas): ");
     scanf("%f", &tiempo_trabajado);
     printf("Ingrese el valor por hora: ");
     scanf("%f", &valor_hora);
-    float sueldo = valor_hora * tiempo_trabajado;
+    sueldo = valor_hora * tiempo_trabajado;
     printf("El sueldo del operario es %.2f pesos\n", sueldo);
     return 0;
 }

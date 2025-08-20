f07c179a9fc2b19fc4a67fbbbef2e4da0a636ba6
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-20 14:17:06 -0300

    Create TP1_B9.c

---
+++ TP1_B9.c
@@ -0,0 +1,14 @@
+// B9. Ingresar el tiempo trabajado por un operario y considerando que el valor por hora es de 10 pesos, calcular su sueldo.
+
+#include <stdio.h>
+
+int main () {
+    int valor_hora = 10;
+    float sueldo, horas;
+    printf("Ejercicio 9: Calcular sueldo de un operario con valor de 10 pesos la hora\n");
+    printf("Ingrese el tiempo trabajado (en horas): ");
+    scanf("%d", &horas);
+    float sueldo = valor_hora * horas;
+    printf("El sueldo del operario es %.2f pesos\n\n", sueldo);
+    return 0;
+}

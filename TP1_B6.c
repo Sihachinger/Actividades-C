8d2dfbd2075e13d39410582929183156d1578121
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-20 14:02:10 -0300

    Create TP1_B6.c

---
+++ TP1_B6.c
@@ -0,0 +1,16 @@
+// B6. Ingresar los lados de un rectángulo y calcular su diagonal principal, superficie y perímetro.
+
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    float base, altura, diagonal, superficie, perimetro;
+    printf("Ejercicio 6: Calcular diagonal, superficie y perímetro de un rectángulo\n");
+    printf("Ingrese la base y la altura del rectángulo: ");
+    scanf("%f %f", &base, &altura);
+    diagonal = (float)sqrt(base * base + altura * altura);
+    superficie = base * altura;
+    perimetro = 2 * (base + altura);
+    printf("La diagonal es %.2f, la superficie es %.2f y el perímetro es %.2f\n", diagonal, superficie, perimetro);
+    return 0;
+}

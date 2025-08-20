0b581bd9785b29637311c125a1dbc355e0c7a91c
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-20 14:49:22 -0300

    Changed variables to float

--- TP1_B2.c
+++ TP1_B2.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 
 int main () {
-    int num1, num2, num3, suma_total;
+    float num1, num2, num3, suma_total;
     printf("Ejercicio 2: Sumar tres valores\n");
     printf("Ingrese tres valores enteros: ");
     scanf("%d %d %d", &num1, &num2, &num3);
     suma_total = num1 + num2;
     suma_total = suma_total + num3;
-    printf("La suma total de %d, %d y %d es %d\n\n", num1, num2, num3, suma_total);
+    printf("La suma total de %.2f, %.2f y %.2f es %.2f\n", num1, num2, num3, suma_total);
     return 0;
 }

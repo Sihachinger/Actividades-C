ed879cbadfbfeadfb73a032860ad2982af8be65d
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-23 23:42:28 -0300

    Update TP1_B10.c

--- TP1_B10.c
+++ TP1_B10.c
@@ -1,4 +1,7 @@
-//B10. Una concesionaria de autos desea liquidar el sueldo a cada vendedor pagando $500 por mes más un plus del 10% del precio sobre cada vehículo vendido y un valor constante de 50 pesos por cada uno de ellos. Se ingresa el valor del vehículo y cuántos vehículos de ese tipo vendió. Calcular su sueldo e imprimirlo.
+/*B10. Una concesionaria de autos desea liquidar el sueldo a cada vendedor pagando $500 por mes más un
+plus del 10% del precio sobre cada vehículo vendido y un valor constante de 50 pesos por cada uno de
+ellos. Se ingresa el valor del vehículo y cuántos vehículos de ese tipo vendió. Calcular su sueldo e
+imprimirlo.*/
 
 #include <stdio.h>
 

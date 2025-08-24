4f6fe972b569b8c5cc4076ffdf017da6c2aa6fb4
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-24 00:21:45 -0300

    Buffer update

--- TP1_B1.c
+++ TP1_B1.c
@@ -1,20 +1,27 @@
-// B1. Ingresar dos valores enteros e imprimir la suma.
-
 #include <stdio.h>
 
-int main () {
-    int num1, num2;
-    char validacion;
-    printf("Ejercicio 1: Sumar dos valores enteros\n");
-    while (1) {
-        printf("Ingrese dos valores enteros: ");
-        if (scanf("%d %d%c", &num1, &num2, &validacion) == 3 && validacion == '\n') {
-            break;
-        } else {
-            printf("Entrada inválida. Asegúrese de ingresar dos enteros sin decimales.\n");
-            while (getchar() != '\n');
+int main (void) {
+    float precio_vehiculo, sueldo_vendedor;
+    int cantidad_vendidos, validacion;
+    char buffer;
+
+    printf("Ejercicio 10: Calcular sueldo de un vendedor de autos\n");
+
+    do {
+        printf("Ingrese el precio del vehículo y la cantidad vendida separados por un espacio: ");
+        validacion = scanf("%f %d%c", &precio_vehiculo, &cantidad_vendidos, &buffer);
+
+        if (validacion != 3 || cantidad_vendidos < 0 || buffer != '\n') {
+            // Limpiar solamente si hubo desborde en la línea o si scanf no tomó los 3
+            if (validacion != 3 || buffer != '\n') {
+                int ch;
+                while ((ch = getchar()) != '\n' && ch != EOF) { /* descartar */ }
+            }
+            printf("La cantidad ingresada no es válida, inténtelo nuevamente.\n");
         }
-    }
-    printf("La suma de %d y %d es %d\n", num1, num2, num1 + num2);
+    } while (validacion != 3 || cantidad_vendidos < 0 || buffer != '\n');
+
+    sueldo_vendedor = 500 + (0.1f * precio_vehiculo * cantidad_vendidos) + (50.0f * cantidad_vendidos);
+    printf("El sueldo del vendedor es %.2f pesos, habiendo vendido %d\n", sueldo_vendedor, cantidad_vendidos);
     return 0;
 }

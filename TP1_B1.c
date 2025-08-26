7822f43a4aa59bdfaa1eed1c0a45e00386f76a9e
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-26 16:36:14 -0300

    code refactoring

--- TP1_B1.c
+++ TP1_B1.c
@@ -1,22 +1,29 @@
+// B1. Ingresar dos valores enteros e imprimir esta suma.
+
 #include <stdio.h>
 
-int main (void) {
-    float precio_vehiculo, sueldo_vendedor;
-    int cantidad_vendidos, validacion;
+// Función para pedir y validar precio y cantidad
+void pedir_datos(float *precio_vehiculo, int *cantidad_vendidos) {
+    int validacion;
     char buffer;
-    printf("Ejercicio 10: Calcular sueldo de un vendedor de autos\n");
-    do {
-        printf("Ingrese el precio del vehículo y la cantidad vendida separados por un espacio: ");
-        validacion = scanf("%f %d%c", &precio_vehiculo, &cantidad_vendidos, &buffer);
-
-        if (validacion != 3 || cantidad_vendidos < 0 || buffer != '\n') {
-            if (validacion != 3 || buffer != '\n') {
-                int ch;
-                while ((ch = getchar()) != '\n' && ch != EOF) { /* descartar */ }
-            }
-            printf("La cantidad ingresada no es válida, inténtelo nuevamente.\n");
+    printf("Ingrese el precio del vehículo y la cantidad vendida separados por un espacio: ");
+    validacion = scanf("%f %d%c", precio_vehiculo, cantidad_vendidos, &buffer);
+    while (validacion != 3 || *cantidad_vendidos < 0 || *precio_vehiculo < 0 || buffer != '\n') {
+        if (validacion != 3 || buffer != '\n') {
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF) { /* descartar */ }
         }
-    } while (validacion != 3 || cantidad_vendidos < 0 || buffer != '\n');
+        printf("La cantidad ingresada no es válida, inténtelo nuevamente.\n");
+        printf("Ingrese el precio del vehículo y la cantidad vendida separados por un espacio: ");
+        validacion = scanf("%f %d%c", precio_vehiculo, cantidad_vendidos, &buffer);
+    }
+}
+
+int main(void) {
+    float precio_vehiculo, sueldo_vendedor;
+    int cantidad_vendidos;
+    printf("Ejercicio 10: Calcular sueldo de un vendedor de autos\n");
+    pedir_datos(&precio_vehiculo, &cantidad_vendidos);
     sueldo_vendedor = 500 + (0.1f * precio_vehiculo * cantidad_vendidos) + (50.0f * cantidad_vendidos);
     printf("El sueldo del vendedor es %.2f pesos, habiendo vendido %d\n", sueldo_vendedor, cantidad_vendidos);
     return 0;

a81e9dadf7378986016e62361bd115fe0c7b5cc8
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-08-31 13:58:25 -0300

    correction of instructions and exercises, implementation of colors in the terminal

--- TP1_B1.c
+++ TP1_B1.c
@@ -1,30 +1,27 @@
-// B1. Ingresar dos valores enteros e imprimir esta suma.
-
 #include <stdio.h>
 
-// Función para pedir y validar precio y cantidad
-void pedir_datos(float *precio_vehiculo, int *cantidad_vendidos) {
-    int validacion;
-    char buffer;
-    printf("Ingrese el precio del vehículo y la cantidad vendida separados por un espacio: ");
-    validacion = scanf("%f %d%c", precio_vehiculo, cantidad_vendidos, &buffer);
-    while (validacion != 3 || *cantidad_vendidos < 0 || *precio_vehiculo < 0 || buffer != '\n') {
-        if (validacion != 3 || buffer != '\n') {
-            int ch;
-            while ((ch = getchar()) != '\n' && ch != EOF) { /* descartar */ }
-        }
-        printf("La cantidad ingresada no es válida, inténtelo nuevamente.\n");
-        printf("Ingrese el precio del vehículo y la cantidad vendida separados por un espacio: ");
-        validacion = scanf("%f %d%c", precio_vehiculo, cantidad_vendidos, &buffer);
+// Funcion para validar y obtener dos enteros
+int obtener_enteros(int *n1, int *n2) {
+    char validacion;
+    // Retorna 1 si lee dos enteros, de lo contrario 0 y limpia el búfer
+    if (scanf("%d %d%c", n1, n2, &validacion) == 3 && validacion == '\n') {
+        return 1;
+    } else {
+        while (getchar() != '\n');
+        return 0;
     }
 }
 
-int main(void) {
-    float precio_vehiculo, sueldo_vendedor;
-    int cantidad_vendidos;
-    printf("Ejercicio 10: Calcular sueldo de un vendedor de autos\n");
-    pedir_datos(&precio_vehiculo, &cantidad_vendidos);
-    sueldo_vendedor = 500 + (0.1f * precio_vehiculo * cantidad_vendidos) + (50.0f * cantidad_vendidos);
-    printf("El sueldo del vendedor es %.2f pesos, habiendo vendido %d\n", sueldo_vendedor, cantidad_vendidos);
+// Función principal
+int main() {
+    int num1, num2;
+    printf("Ejercicio 1: Sumar dos valores enteros\n");
+    
+    // Bucle do-while para solicitar y validar la entrada
+    do {
+        // Buscamos como aplicar colores en la terminal por medio de códigos ANSI, para resaltar que se esperan enteros
+        printf("Ingrese dos valores \033[36menteros\033[0m:  ");
+    } while (!obtener_enteros(&num1, &num2));
+    printf("La suma de %d y %d es %d\n", num1, num2, num1 + num2);
     return 0;
 }

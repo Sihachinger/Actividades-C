1a8c5ebab784d867feb38bf28f94b7abbebc537f
Victor Sihachinger <vicsih@gmail.com>
2025-09-02 20:44:10 -0300

    Exercise 2

--- TP2/TP2_2.c
+++ TP2/TP2_2.c
@@ -1,1 +1,29 @@
 //2. Ingresar un valor, indicar e imprimir si es positivo, negativo o cero.
+
+#include <stdio.h>
+
+int main() {
+    float valor;
+
+    printf("Ingrese un valor: ");
+    scanf("%f", &valor);
+
+    if (valor > 0) {
+        printf("El valor es positivo.\n");
+    } else if (valor < 0) {
+        printf("El valor es negativo.\n");
+    } else {
+        printf("El valor es cero.\n");
+    }
+
+    return 0;
+}
+
+/*
+3           Incluir Bibliotecas
+5           Inicio del programa
+8 - 9       Imprimir y pedir el valor
+11 - 17     Analizar el valor e imprimir el resultado
+19          Return
+20          Fin del programa
+*/

3168f0d0faab89bdb65207907a64c9c80445982f
Victor Sihachinger <vicsih@gmail.com>
2025-09-02 20:36:33 -0300

    Exercise 1

--- TP2/TP2_1.c
+++ TP2/TP2_1.c
@@ -1,1 +1,20 @@
 //1. Ingresar dos valores, indicar e imprimir si son iguales.
+
+#include <stdio.h>
+
+int main() {
+    float valor1, valor2;
+
+    printf("Ingrese el primer valor: ");
+    scanf("%f", &valor1);
+    printf("Ingrese el segundo valor: ");
+    scanf("%f", &valor2);
+
+    if (valor1 == valor2) {
+        printf("Los valores son iguales.\n");
+    } else {
+        printf("Los valores son diferentes.\n");
+    }
+    
+    return 0;
+}

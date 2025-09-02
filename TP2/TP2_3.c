5dd80c16e710e81bf2d2193e3ffabed8b9f58f72
Victor Sihachinger <vicsih@gmail.com>
2025-09-02 20:51:42 -0300

    Exercise 3

--- TP2/TP2_3.c
+++ TP2/TP2_3.c
@@ -1,1 +1,24 @@
 //3. Ingresar los tres lados de un triangulo e indicar que tipo de triangulo es.
+
+#include <stdio.h>
+
+int main() {
+    float lado1, lado2, lado3;
+
+    printf("Ingrese el primer lado: ");
+    scanf("%f", &lado1);
+    printf("Ingrese el segundo lado: ");
+    scanf("%f", &lado2);
+    printf("Ingrese el tercer lado: ");
+    scanf("%f", &lado3);
+
+    if (lado1 == lado2 && lado2 == lado3) {
+        printf("El triángulo es equilátero, todos los lados son iguales.\n");
+    } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+        printf("El triángulo es isósceles, dos lados son iguales.\n");
+    } else {
+        printf("El triángulo es escaleno, todos los lados son diferentes.\n");
+    }
+
+    return 0;
+}

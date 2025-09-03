5d230b7d067fedb92f204659773d9eae68790149
Victor Sihachinger <vicsih@gmail.com>
2025-09-02 21:57:41 -0300

    Exercise 6

--- TP2/TP2_6.c
+++ TP2/TP2_6.c
@@ -1,1 +1,35 @@
 // 6. Ingresar la edad y la altura de dos personas, indicar e imprimir la estatura del de mayor edad.
+
+#include <stdio.h>
+
+int main() {
+    int edad1, edad2;
+    float altura1, altura2;
+
+    printf("Ingrese la edad y altura de la primera persona: ");
+    scanf("%d %f", &edad1, &altura1);
+    printf("Ingrese la edad y altura de la segunda persona: ");
+    scanf("%d %f", &edad2, &altura2);
+
+    if (edad1 > 0 && altura1 > 0 && edad2 > 0 && altura2 > 0) {
+        if (edad1 > edad2) {
+            printf("La altura de la persona mayor es: %.2f\n", altura1);
+        } else {
+            printf("La altura de la persona mayor es: %.2f\n", altura2);
+        }
+    } else {
+        printf("Entrada no válida. Por favor, ingrese valores positivos.\n");
+    }
+
+    return 0;
+}
+
+/*
+3           Incluir Bibliotecas
+5           Inicio del programa
+6 - 7       Declarar variables
+9 - 12      Imprimir y pedir los valores
+14 - 22     Comparar edades para mostrar la altura del mayor
+24          Return
+25          Fin del programa
+*/

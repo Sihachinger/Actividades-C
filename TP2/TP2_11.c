ae512b45bf360f2842c080b101d619a0a3b08a31
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-09-03 18:52:51 -0300

    Exercise 11

--- TP2/TP2_11.c
+++ TP2/TP2_11.c
@@ -1,1 +1,30 @@
 // 11. Ingresar las horas trabajadas por un empleado y su categoría, calcular su sueldo sabiendo que los empleados de la categoría 1 cobran $50, la 2 cobra $ 70 y la 3 cobra $ 80
+
+
+#include <stdio.h>
+
+int main() {
+    float sueldo;
+    int categoria, antiguedad;
+
+    printf("Ingrese la categoría del empleado y las horas trabajadas, separado por espacios: ");
+    scanf("%f %d", &horas, &categoria);
+
+    switch (categoria) {
+        case 1:
+            sueldo = horas * 50;
+            break;
+        case 2:
+            sueldo = 70 * 50;
+            break;
+        case 3:
+            sueldo = 80 * 50;
+            break;
+        default:
+            printf("Categoria no valida.\n");
+            return 0;
+    }
+
+    printf("El sueldo del emleaado es de: %.2f\n", sueldo);
+    return 0;
+}

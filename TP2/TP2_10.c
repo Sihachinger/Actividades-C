96840427a8b9eb99ad02e6709dbf44742cb99b92
Victor Sihachinger <102450070+Sihachinger@users.noreply.github.com>
2025-09-03 19:51:05 -0300

    Bugs fixed

--- TP2/TP2_10.c
+++ TP2/TP2_10.c
@@ -1,6 +1,6 @@
 // 10. Sobre los datos del ejercicio anterior imprimir los sueldos de los empleados con más de 5 años de antigüedad
 
-include <stdio.h>
+#include <stdio.h>
 
 int main() {
     int cantidad_trabajadores;
@@ -15,10 +15,12 @@ int main() {
         printf("Ingrese el nombre, sueldo, categoría y antigüedad del trabajador %d separado por espacios:\n", i + 1);
         scanf("%s %f %d %d", nombre[i], &sueldo[i], &categoria[i], &antiguedad[i]);
     }
-
+    printf("Trabajadores con más de 5 años de antigüedad:\n");
     for (int i = 0; i < cantidad_trabajadores; i++) {
-        If (antigüedad[i] >= 5){
+        if (antiguedad[i] >= 5) {
             printf("Trabajador %d: %s, Sueldo: %.2f, Categoría: %d, Antigüedad: %d años\n", i + 1, nombre[i], sueldo[i], categoria[i], antiguedad[i]);
+        } else {
+            printf("Trabajador %d: %s no tiene más de 5 años de antigüedad.\n", i + 1, nombre[i]);
         }
     }
 

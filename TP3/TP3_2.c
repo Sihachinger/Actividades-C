7ed3707c421f6badc95a3269cd65d920d51751e2
Victor Sihachinger <vicsih@gmail.com>
2025-09-09 17:39:13 -0300

    Exercise 2

--- TP3/TP3_2.c
+++ TP3/TP3_2.c
@@ -1,1 +1,18 @@
-//Ingresar10 valores, indicar e imprimir cuántos son positivos, cuántos negativos y cuántos ceros
+//Ingresar 10 valores, indicar e imprimir cuántos son positivos, cuántos negativos y cuántos ceros
+
+#include <stdio.h>
+
+int main () {
+    float valores, positivos, negativos, ceros;
+    for ( int i = 0; i < 10; i++) {
+        printf("Ingrese el número %d: ", i);
+        scanf("%f", &valores);
+        if (valores > 0) {
+            positivos++;
+        } else if (valores < 0) {
+            negativos++;
+        } else {
+            ceros++;
+        }
+    }
+}
